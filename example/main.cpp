fad47c3b12f8cc2b6e8d273cd266e3249f4d6d92
IamBogdan <admiral01@mail.ua>
2022-07-10 22:24:40 +0300

    fix: fixes the example adds a check on the number of arguments; changes the connection of the module.

--- example/main.cpp
+++ example/main.cpp
@@ -1,21 +1,25 @@
-#include "../finder/finder.hpp"
+#include "finder.hpp"
 
 int main(int argc, const char* argv[])
 {
-  if(argc > 1){
-    std::cout << "printPathFile:\n";
-    finder::printPathFile(argv[1]);
 
-    std::cout << "\nsearchFileByName:\n";
-    std::string res;
-    if(finder::searchFileByName("/", argv[1], 8, res))
-      std::cout << res << '\n';
-    else
-      std::cout << "File not found\n";
-  }
-  else{
+  if(argc == 1){
     std::cout << "Enter the name of the file you want to find.\n";
+    return 1;
   }
+  else if(argc == 2){
+    std::cout << "Enter the number of threads.\n";
+    return 1;
+  }
+  
+  std::string file_name = argv[1];
+  int thread_count = atoi(argv[2]);
+  std::string result;
+
+  if(finder::searchFileByName("/", file_name, thread_count, result))
+    std::cout << result << '\n';
+  else
+    std::cout << "File not found\n";
 
   return 0;
 }
